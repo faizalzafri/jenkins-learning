6b9f07d3868cf4ebe5df6fb12d16772275709389
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-23 22:49:07 +0530

    Update script.sh

--- jenkins-aws/script.sh
+++ jenkins-aws/script.sh
@@ -10,7 +10,7 @@ AWS_SECRET=$4
 BUCKET_NAME=$5
 
 mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
-export AWS_ACCESS_KEY_ID=JSHDBJHFBSBDJHSBD && \
+export AWS_ACCESS_KEY_ID= && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Uploading your $BACKUP backup" && \
 aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
